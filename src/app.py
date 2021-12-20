7010065f7fc49658a3e244eb0dc8e8f9af6f3fc4
dilky97 <2017cs039@stu.ucsc.cmb.ac.lk>
2021-12-20 13:41:59 +0530

    Added support to choose classes

--- src/app.py
+++ src/app.py
@@ -16,46 +16,97 @@ async def serve(q:Q):
     await q.page.save()
 
 
-async def display_detections(q,image_path):
+async def display_detections(q):
+    """
+    Display the image with bounding boxes for detected objects
+    :param q: Query argument from the H2O Wave server 
+    :return None
+    """
+    image_path = "./data/"+ q.client.uploaded_path[2:]
 
-    image_path = "./data/"+ image_path[2:]
+    # If user has not picked what objects to display, display all the detections
+    if not q.client.picked_classes:
+        q.page['content'] = ui.form_card(
+            box = ui.box('content'),
+            title = "Object Detector",
+            items = [
+                ui.text("No objects picked. Displaying all"),
+                ui.picker(name='object_picker', label='Pick objects to detect', width = '50%', trigger=True,
+                    choices=[ui.choice(thing, thing) for thing in q.app.object_detector.classes_names] , values=[]),
+            ],
+        )
 
-    q.page['uploaded_image'] = ui.image_card(
-        box = ui.box('content', width='900px', height='500px'),
-        # box = 'content',
-        title = '',
-        type = 'png',    
-        image = q.app.object_detector.detect_objects(image_path),
-        # path=image_path
-    )
+        q.page['uploaded_image'] = ui.image_card(
+            box = ui.box('content', width='900px', height='550px'),
+            title = '',
+            type = 'png',    
+            image = q.app.object_detector.detect_objects(image_path,q.client.picked_classes),
+            # path=image_path
+        )
+
+    # Display the selected object types
+    else:
+        q.page['content'] = ui.form_card(
+                box = ui.box('content'),
+                title = "Object Detector",
+                items = [
+                    ui.picker(name='object_picker', label='Pick objects to detect', width = '50%', trigger=True,
+                        choices=[ui.choice(thing, thing) for thing in q.app.object_detector.classes_names] , values=[]),
+
+                ],
+            )
+
+        q.page['uploaded_image'] = ui.image_card(
+            box = ui.box('content', width='900px', height='550px'),
+            title = '',
+            type = 'png',    
+            image = q.app.object_detector.detect_objects(image_path,q.client.picked_classes),
+            # path=image_path
+        )
+    
+    q.client.picked_classes = []
     await q.page.save()
 
 
 async def set_sample_path(q):
+    """
+    Path handler for sample images.
+    
+    """
     option = q.client.dropdown_option
-    if  option == 'nyc':
+    if option == 'nyc':
         sample_image_path = './static/nyc.jpg'
-        uploaded_path, = await q.site.upload([sample_image_path])
-        await display_detections(q,uploaded_path)
+        q.client.uploaded_path, = await q.site.upload([sample_image_path])
+        await display_detections(q)
     
     if option == 'horserace':
         sample_image_path = './static/horserace.jpg'
-        uploaded_path, = await q.site.upload([sample_image_path])
-        await display_detections(q,uploaded_path)
+        q.client.uploaded_path, = await q.site.upload([sample_image_path])
+        await display_detections(q)
 
     
 async def initialize_app(q:Q):
+    """
+    Initialize the app prior to first usage
+    :param q: Query argument from the H2O Wave server
+    :return: None
+    """
+    logging.info("Initializing the app.")
+
     if not q.app.initialized:
         q.app.initialized = True
         q.app.object_detector = ObjectDetector()
-        q.client.dropdown_option = 'nyc'
+        q.client.dropdown_option = 'horserace'
+        q.client.picked_classes = []
+        q.client.uploaded_path = ''
 
         logging.info("App initialization complete")
 
     if not q.client.initialized:
-        # setup app
+        
         q.client.initialized = True
 
+        #UI Layout
         q.page['meta'] = ui.meta_card(box='', layouts=[    
             ui.layout(        
                 breakpoint='xl', 
@@ -76,10 +127,11 @@ async def initialize_app(q:Q):
         q.page['header'] = ui.header_card(box='header', title='Object Detector', subtitle='Using Detectron2')
 
         q.page['sidebar'] = ui.form_card(
-            box = ui.box(zone='sidebar'),
+            box = 'sidebar',
             title ="Upload an Image",
             items =[
-                ui.file_upload(name='image_upload', label='', multiple=False, file_extensions=['jpg', 'png']),
+                ui.file_upload(name='file_upload', label='Upload an Image', multiple=False, file_extensions=['jpg', 'png','mp4']),
+                ui.separator(label=''),
                 ui.dropdown(name='dropdown_images', label='Choose a sample image', value=q.client.dropdown_option, choices=[
                     ui.choice(name="nyc", label="NYC.jpg"),
                     ui.choice(name="horserace", label="Horse Race.jpg")
@@ -88,37 +140,34 @@ async def initialize_app(q:Q):
             ],
         )
 
-        q.page['content'] = ui.section_card(
-            box = 'content',
-            title = "Object Detector",
-            subtitle = '',
-            items=[
-                # ui.toggle(name='search', label="A", value=True),
-                # ui.dropdown(name='task', label='Choose the task', value='option0', choices=[
-                #     ui.choice(name="object_detection", label="Object Detection"),
-                #     ui.choice(name="instance_segmentation", label="Instance Segmentation")
-                # ]),
-                # ui.date_picker(name='target_date', label='', value='2020-12-25'),
-            ],
+        q.page['content'] = ui.section_card(    
+            box='content',    
+            title='Object Detector',    
+            subtitle='Show Results'
         )
 
-        q.page['footer'] = ui.footer_card(box='footer', caption='(c) 2021 H2O.ai. All rights reserved.')
+
+        q.page['footer'] = ui.footer_card(box='footer', caption="Made with H2O Wave. Images downloaded from pexels.com")
 
 
-    if q.args.image_upload:
-        # await add_progress(q)
-        uploaded_path = q.args.image_upload[0]
-        # image_path = await q.site.download(uploaded, './temp/')
-        print(uploaded_path)
-
-        await display_detections(q,uploaded_path)
+    # Runs when user uploads a custom image
+    if q.args.file_upload:
+        q.client.uploaded_path = q.args.file_upload[0]
+        await display_detections(q)
     
+    # Runs when user chooses a sample image from the dropdown
     if q.args.dropdown_images:
         q.client.dropdown_option = q.args.dropdown_images
 
     if q.args.show_image:
         await set_sample_path(q)
+    
+    # Runs when user chooses which objects to detect
+    if q.args.object_picker:
+        q.client.picked_classes = q.args.object_picker
+        await display_detections(q)
 
+    
 
 
 
--- src/object_detector.py
+++ src/object_detector.py
@@ -9,9 +9,13 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-CLASSES = [0, 17]
 
 class ObjectDetector:
+
+    """
+    Program to detect objects in an image using pretrained Faster RCNN network in Detectron2 Library.
+    Adapted from https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5
+    """
 
     def __init__(self) -> None:
         self.cfg = get_cfg()
@@ -22,6 +26,8 @@ class ObjectDetector:
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
         # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
         self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+
+        self.classes_names = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).thing_classes
 
         self.predictor = DefaultPredictor(self.cfg)
 
@@ -38,33 +44,42 @@ class ObjectDetector:
 
         return img_str
 
-    # def discriminate(self, outputs):
-    #     pred_classes = np.array(outputs['instances'].pred_classes)
-    #     mask = np.isin(pred_classes, CLASSES)
-    #     idx = np.nonzero(mask)
+    def __pick_classes(self, outputs, picked_classes):
+        pred_classes = np.array(outputs['instances'].pred_classes)
+        mask = np.isin(pred_classes, picked_classes)
+        idx = np.nonzero(mask)
         
-    #     # Get Instance values as a dict and leave only the desired ones
-    #     out_fields = outputs['instances'].get_fields()
-    #     for field in out_fields:
-    #         out_fields[field] = out_fields[field][idx]
+        # Get Instance values as a dict and leave only the desired ones
+        out_fields = outputs['instances'].get_fields()
+        for field in out_fields:
+            out_fields[field] = out_fields[field][idx]
 
-    #     return outputs
+        return outputs
 
 
-    def detect_objects(self, image_path):
+    def detect_objects(self, image_path, picked_classes):
         img = Image.open(image_path).convert('RGB')
         img = np.array(img)
 
         # self.cfg, predictor = load_predictor()
         outputs = self.predictor(img)
-        result_img = self.__visualize_output(img, outputs)
+        if not picked_classes:
+            result_img = self.__visualize_output(img, outputs)
+        else:
+            # pred_classes = np.array(outputs['instances'].pred_classes)
+            mask = np.isin(self.classes_names, picked_classes)
+            # mask = np.isin(pred_classes, picked_classes)
+            class_idxs = np.nonzero(mask)
+            picked_outputs = self.__pick_classes(outputs, class_idxs)
+            result_img = self.__visualize_output(img, picked_outputs)
 
         assert (type(result_img)==str), "Output needs to be a String" 
 
         return result_img
 
 def main():
-    predict = ObjectDetector().detect_objects('./static/nyc.jpg')
+    detector = ObjectDetector()
+    predict = detector.detect_objects('./static/nyc.jpg', ['person'])
 
     im_bytes = base64.b64decode(predict)   # im_bytes is a binary image
     im_file = BytesIO(im_bytes)  
